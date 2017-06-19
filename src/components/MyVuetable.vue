de0881fc45863b933d278a81f23d9ec55a680cfc
zee-german <franz.lutzenberger@gmail.com>
2017-06-19 12:52:02 -0400

    added more pagination info

--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -1,31 +1,47 @@
 <template>
   <div class="ui container">
 
+    <div class="vuetable-pagination ui basic segment grid">
+      <vuetable-pagination-info ref="paginationInfoTop">
+      </vuetable-pagination-info>
+      <vuetable-pagination
+      ref="paginationTop"
+      @vuetable-pagination:change-page="onChangePage"
+      >
+    </vuetable-pagination>
+  </div>
   <vuetable
     ref="vuetable"
     api-url="https://vuetable.ratiw.net/api/users"
     :fields="fields"
     pagination-path=""
+    :per-page="20"
     @vuetable:pagination-data="onPaginationData"
   ></vuetable>
-  <vuetable-pagination
+  <div class="vuetable-pagination ui basic segment grid">
+    <vuetable-pagination-info ref="paginationInfo">
+    </vuetable-pagination-info>
+    <vuetable-pagination
     ref="pagination"
     @vuetable-pagination:change-page="onChangePage"
     >
   </vuetable-pagination>
 </div>
+</div>
 </template>
 
 <script>
 import Vuetable from 'vuetable-2/src/components/Vuetable'
-import VuetablePagination from 'vuetable-2/src/components/VuetablePaginationDropdown'
+import VuetablePagination from 'vuetable-2/src/components/VuetablePagination'
+import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo'
 import accounting from 'accounting'
 import moment from 'moment'
 
 export default {
   components: {
     Vuetable,
-    VuetablePagination
+    VuetablePagination,
+    VuetablePaginationInfo
   },
   data () {
     return {
@@ -56,7 +72,8 @@ export default {
           name: 'salary',
           titleClasss: 'center aligned',
           dataClass: 'right aligned',
-          callback: 'formatNumber'
+          callback: 'formatNumber',
+          visible: false
         }
       ]
     }
@@ -79,6 +96,10 @@ export default {
         : moment(val, 'YYYY-MM-DD').format(fmt)
       },
       onPaginationData (paginationData) {
+        this.$refs.paginationInfoTop.setPaginationData(paginationData)
+        this.$refs.paginationTop.setPaginationData(paginationData)
+
+        this.$refs.paginationInfo.setPaginationData(paginationData)
         this.$refs.pagination.setPaginationData(paginationData)
       },
       onChangePage (page) {
