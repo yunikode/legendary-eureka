e0400d52a7b6c8b1d9f624d4316fe2bcc9aa0850
zee-german <franz.lutzenberger@gmail.com>
2017-06-19 10:44:10 -0400

    display simple data from API endpoint

---
+++ src/components/MyVuetable.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <vuetable
+    ref="vuetable"
+    api-url="https://vuetable.ratiw.net/api/users"
+    :fields="fields"
+  ></vuetable>
+</template>
+
+<script>
+import Vuetable from 'vuetable-2/src/components/Vuetable'
+export default {
+  components: {
+    Vuetable
+  },
+  data () {
+    return {
+      fields: [
+        'name', 'email', 'birthdate', 'address.line1', 'address.line2', 'address.zipcode'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
